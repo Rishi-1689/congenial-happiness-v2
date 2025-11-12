8451569526cac71ca81413ce3aec02fcff207cad
Rishi-1689 <rishinjgd@gmail.com>
2025-11-13 00:14:54 +0100

    styled using css and bootstrap

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'company': 'ABC-Corp',
+        'position': 'Security',
+        'salary': '$100'
+    },
+    {
+        'id': 2,
+        'company': 'Umbrella-Corp',
+        'position': 'Umbrella-Maker',
+        'salaray': '$400'
+    },
+    {
+        'id': 3,
+        'company': 'Skynet',
+        'position': 'Terminator',
+        'salary': '$2',
+    }
+]
+
 
 @app.route("/")
 def hello_world():
-    return "Hello World"
+    return render_template('index.html', jobs=JOBS)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
